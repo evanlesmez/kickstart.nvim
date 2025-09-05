fb81e3fbd68670beb0d8829cc9ba44a933340cfc
Evan Lesmez <evan.lesmez@gmail.com>
2025-09-05 16:55:30 -0400

    add neogit

--- init.lua
+++ init.lua
@@ -950,6 +950,17 @@ require('lazy').setup({
       { '<leader>vs', '<cmd>LoveStop<cr>', ft = 'lua', desc = 'Stop LÖVE' },
     },
   },
+  {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- required
+      'sindrets/diffview.nvim', -- optional - Diff integration
+      'nvim-telescope/telescope.nvim',
+    },
+    opts = {
+      graph_style = 'unicode',
+    },
+  },
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
